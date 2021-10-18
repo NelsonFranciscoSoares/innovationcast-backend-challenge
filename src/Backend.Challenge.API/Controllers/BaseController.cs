22d5dbecda5fa384aa31c66eac4daf3465aa8b15
Nelson Francisco Vicente Soares <nelson.francisco@dbccompany.com.br>
2021-10-18 12:07:59 -0300

    - added services project

---
+++ src/Backend.Challenge.API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Challenge.API.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+      protected BaseController() { }
+    }
+}
---
+++ src/Backend.Challenge.API/Controllers/ComentarioController.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Challenge.Application.DataTransferObjets;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Challenge.API.Controllers
+{
+    [Route("api/entidade/{entidadeId:guid}/[controller]")]
+    public class ComentarioController : BaseController
+    {
+        public ComentarioController() { }
+
+
+        [HttpPost("adicionar-novo")]
+        public Task AdicionaComentario(Guid entidadeId, ComentarioDTO comentarioDTO)
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpPut("actualizar-existente/{comentarioId:guid}")]
+        public Task ActualizaComentario(Guid comentarioId)
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpDelete("remover/{comentarioId:guid}")]
+        public Task ActualizaComentario(Guid entidadeId, Guid comentarioId)
+        {
+            return Task.CompletedTask;
+        }
+
+        [HttpGet("obter-comentarios")]
+        public Task ObterComentario(Guid entidadeId, [FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 1)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
